2ac5058ff27d132cb2c2aee5d495305ddf98193c
Harshit Joshi <harshitjoshi450@gmail.com>
2022-09-08 18:59:52 +0530

    More params in figure and img

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ a=np.linspace(0,10,11)
 b=a**4
 x=np.arange(0, 10)
 y=2*x
-fig=plt.figure()
+fig=plt.figure(figsize=(3,3), dpi=100)
 
 #Large Axes
 axes1=fig.add_axes([0, 0, 1, 1])
-axes1.set_xlim(0, 8)
-axes1.set_ylim(0,8000)
+axes1.set_xlim(0, 10)
+axes1.set_ylim(0,10000)
 axes1.set_xlabel("A")
 axes1.set_ylabel("B")
 axes1.set_title("Power of 4")
@@ -25,5 +25,7 @@ axes1.set_ylabel("B")
 axes1.set_title("Zoomed IN")
 axes2.plot(x, y)
 
+#Saving fig
+fig.savefig('newfig.png', bbox_inches='tight')
 
 plt.show()
