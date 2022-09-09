ea63d1318bf68c4ff155094160d935e142ef2b0c
Harshit Joshi <harshitjoshi450@gmail.com>
2022-09-09 22:27:50 +0530

    Legends

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,27 +7,10 @@ b=a**4
 x=np.arange(0, 10)
 y=2*x
 
+fig=plt.figure()
+ax=fig.add_axes([0, 0, 1, 1])
+ax.plot(x,x, label="X v/s X")
+ax.plot(x,x**2, label="X v/s X^2")
+ax.legend()
 
-
-#ONE DIMENSIONAL
-"""
-fig, axes=plt.subplots(nrows=3, ncols=1)
-for ax in axes:
-    ax.plot(x, y)
-"""
-#axes[0].plot(x,y)
-#axes[1].plot(a,b)
-
-
-
-fig, axes= plt.subplots(nrows=2, ncols=2)
-axes[0][0].plot(x, y)
-axes[0][1].plot(x, y)
-axes[1][1].plot(a, b)
-axes[1][0].set_ylabel("Y Label-Empty plot 1.0")
-axes[1][1].set_title("Exponential Plot 1.1")
-fig.suptitle("Subplots Demo Figure Level", fontsize=18)
-plt.tight_layout()
-
-fig.savefig("newsubplot.png", bbox_inches='tight')
 plt.show()
